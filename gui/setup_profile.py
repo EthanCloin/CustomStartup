bb835da1f1312ea0d310bc7d4f9f7b4f02939d0d
Ethan Cloin <psnethan@gmail.com>
2023-01-21 16:41:46 -0500

    First draft of RoutineListView layout as desired

--- gui/setup_profile.py
+++ gui/setup_profile.py
@@ -8,144 +8,77 @@ delete an existing StartupRoutine
 execute a StartupRoutine
 """
 
-from tkinter import Tk, ttk
-from openers.startup import StartupRoutine, WORK_BASICS
+import tkinter as tk
+from openers.startup import WORK_BASICS
+
+# Constants
+BODY_FONT = ("Arial", 18)
+TITLE_FONT = ("Arial", 32)
 
 
 def main():
-    root = Tk()
-    root.title("Self Starter")
-    root.geometry("1200x800")
-    ProfileFrame(root).pack(expand=True, fill="both")
+    root = tk.Tk()
+    win_width = root.winfo_screenwidth() // 2
+    win_height = root.winfo_screenheight() // 2
+
+    root.geometry(f"{win_width}x{win_height}+{win_width // 2}+{win_height // 2}")
+    # {win_width - 150}+{win_height}
+    # print(
+    #     WINDOW_SIZE
+    #     + "+{}+{}".format(
+    #         root.winfo_screenwidth() // 2 - 150, root.winfo_screenheight() // 2 - 75
+    #     )
+    # )
+
+    frame1 = RoutineListView(root)
+    frame1.pack(side="left")
+
+    frame2 = RoutineDetailView(root)
+    frame2.pack(side="right")
 
     root.mainloop()
 
 
-class ProfileFrame(ttk.Frame):
-    def __init__(self, master: Tk, *args, **kwargs):
-        super().__init__(master=master, *args, **kwargs)
-        main_window_label_text = "Your Startup Routines"
+class RoutineListView(tk.Frame):
+    def __init__(self, master, *args, **kwargs) -> None:
+        super().__init__(master, *args, **kwargs)
 
-        child_width = int(master.winfo_width() // (4 / 10))
-        child_height = int(master.winfo_height() // (8 / 10))
-        # print(f"{child_width}x{child_height}")
+        title = tk.Label(self, text="Your Routines", font=TITLE_FONT)
+        title.pack()
 
-        # frame for available routines list
-        RoutinesListView(self, width=500, height=child_height).pack(
-            side="left", expand=True, fill="x"
-        )
-        RoutinesListView(self, width=child_width, height=child_height).pack(
-            side="left", expand=True, fill="y"
-        )
-        # RoutinesListView(self, width=600, height=400).pack(
-        #     side="left", expand=True, fill="both"
-        # )
+        inner_frame = tk.Frame(self, bg="blue")
+        inner_frame.pack(expand=True, fill="both")
 
-        # frame for detail view of selected routine
+        eg_routines = ["first routine", "second", "3rd routine"]
+
+        for r in eg_routines:
+            routine_btn = tk.Button(inner_frame, text=r, font=BODY_FONT)
+            routine_btn.pack(side="top", anchor="nw", expand=True, fill="x")
 
 
-class RoutinesListView(ttk.Frame):
+class RoutineDetailView(tk.Frame):
+    def __init__(self, master, *args, **kwargs) -> None:
+        super().__init__(master, *args, **kwargs)
 
-    routines_list = ["Routine 1", "routine 2", "routine three"]
+        title = tk.Label(self, text="Routine Details", font=TITLE_FONT)
+        title.pack()
 
-    def __init__(self, master, *args, **kwargs):
-        super().__init__(master=master, *args, **kwargs)
+        inner_frame = tk.Frame(self)
+        inner_frame.pack()
 
-        for routine in self.routines_list:
-            ttk.Button(self, text=routine).pack(side="top", expand=True, fill="both")
+        eg_routine = WORK_BASICS
+        app_count = len(eg_routine.applications)
+        site_count = len(eg_routine.websites)
+
+        title_label = tk.Label(inner_frame, text=eg_routine.name, font=BODY_FONT)
+        title_label.pack()
+
+        app_label = tk.Label(inner_frame, text=f"Apps: {app_count}", font=BODY_FONT)
+        app_label.pack(side="left")
+
+        site_label = tk.Label(inner_frame, text=f"Sites: {site_count}", font=BODY_FONT)
+        site_label.pack(side="right")
 
 
 if __name__ == "__main__":
     main()
-
-# from tkinter import Tk
-# from tkinter import ttk
-
-# from openers.startup import StartupRoutine, WORK_BASICS
-
-
-# class ProfileWindow(ttk.Frame):
-#     window_title = "Self Starter"
-#     content_title = "Your Startup Routine"
-
-#     listed_routines: ttk.Frame
-#     selected_routine_detail: ttk.Frame
-
-#     # geometry
-#     min_width, min_height = 600, 400
-
-#     def __init__(self, master, *args, **kwargs):
-#         super().__init__(master=master, *args, **kwargs)
-#         self.listed_routines = RoutinesListView(master=self).pack(
-#             side="left", expand=True
-#         )
-#         self.selected_routine_detail = RoutineDetailView(master=self).pack()
-
-
-# class RoutinesListView(ttk.LabelFrame):
-#     routines_list = ["Routine 1", "routine 2", "routine three"]
-
-#     def __init__(self, master):
-#         super().__init__(master=master)
-#         for idx, routine in enumerate(self.routines_list):
-#             routine_label = ttk.Label(self, text=routine)
-#             routine_label.pack(side="top")
-
-
-# # styler = ttk.Style()
-# # styler.configure("RedFrame.TFrame", background="red")
-
-
-# class RoutineDetailView(ttk.Frame):
-#     details = WORK_BASICS
-
-#     def __init__(self, master):
-#         super().__init__(master=master)
-#         name_group = ttk.Frame(self, style="RedFrame.TFrame").grid()
-#         name_label = ttk.Label(name_group, text="Name: ").grid(row=0, column=0)
-#         name_value = ttk.Entry(
-#             name_group, text=self.details.name, state="DISABLED"
-#         ).grid(row=0, column=1)
-
-#         # info_group
-#         # info_label = ttk.Label(self, text="Info: ").pack(side="top")
-#         # apps_value = ttk.Label(
-#         #     self, text=f"Apps: {len(self.details.applications)}"
-#         # ).pack(side="right")
-#         # sites_value = ttk.Label(self, text=f"Sites: {len(self.details.websites)}").pack(
-#         #     side="right"
-#         # )
-
-
-# def testing():
-#     import tkinter as tk
-
-#     # Create Tkinter Object
-#     root = Tk()
-
-#     # Set Geometry
-#     root.geometry("400x400")
-
-#     # Frame 1
-#     frame1 = tk.Frame(root, bg="black", width=500, height=300)
-#     frame1.pack()
-
-#     # Frame 2
-#     frame2 = tk.Frame(frame1, bg="white", width=100, height=100)
-#     frame2.pack(pady=20, padx=20)
-
-#     # Execute Tkinter
-#     root.mainloop()
-
-
-# def main():
-#     root = Tk()
-#     root.geometry("600x400")
-
-#     # make instance of profile window and call the pack method to layout within root
-#     ProfileWindow(root).pack()
-#     root.mainloop()
-
-
-# if __name__ == "__main__":
-#     main()
